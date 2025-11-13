f90fb4130bc70e2da69e5c32f1e5a2bb2fe6c37c
Muhammad <netflixuser22232@gmail.com>
2025-11-13 18:11:59 +0500

    minor changes

--- lib/providers/blog_provider.dart
+++ lib/providers/blog_provider.dart
@@ -1,8 +1,8 @@
+import 'package:blog_app_flutter/model/blog_model.dart';
+import 'package:flutter/material.dart';
+
 class BlogProvider extends ChangeNotifier {
-
-
-
-    void uploadBlog (BlogModel blog){
-        // use firebase firestore to upload the data
-    }
+  void uploadBlog(BlogModel blog) {
+    // use firebase firestore to upload the data
+  }
 }
