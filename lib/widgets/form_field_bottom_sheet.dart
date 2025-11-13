0278e31f24e8944a87a9d00d764a793cb6808b7b
Your Name <your.email@example.com>
2025-11-13 13:19:35 +0500

    Separate form field for bottom sheet

---
+++ lib/widgets/form_field_bottom_sheet.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class FormFieldBottomSheet extends StatelessWidget {
+  Color cursorColor;
+  final String hintText;
+  Color hintStyleColor;
+  final double fontSize;
+  final FontWeight fontWeight;
+
+  FormFieldBottomSheet({
+    super.key,
+    this.cursorColor = const Color(0xFF90a955),
+    required this.hintText,
+    this.hintStyleColor = const Color(0xFF283618),
+    required this.fontSize,
+    required this.fontWeight,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    var width = size.width;
+    var height = size.height;
+    return Container(
+      margin: EdgeInsets.only(right: 10),
+      padding: EdgeInsets.only(left: 10),
+      width: width * 0.9,
+      height: height * 0.06,
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 226, 234, 134),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: TextFormField(
+        cursorColor: cursorColor,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: hintText,
+          hintStyle: TextStyle(
+            color: hintStyleColor,
+            fontSize: fontSize,
+            fontWeight: fontWeight,
+          ),
+        ),
+      ),
+    );
+  }
+}
